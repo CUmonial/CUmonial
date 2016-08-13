9fa7a2fa057fcf73cd9d498d793227b9d6b7fcb0
CUmonial <indiacantwait@gmail.com>
2016-08-13 21:07:38 +0000

    UI of registration partial

--- demoApp/db/migrate/20160731090300_devise_create_app_users.rb
+++ demoApp/db/migrate/20160731090300_devise_create_app_users.rb
@@ -4,7 +4,7 @@ class DeviseCreateAppUsers < ActiveRecord::Migration
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-
+      t.string :user_name,          null: false, default: ""
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
