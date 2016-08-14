33a918c527b93ffc7dfffb2eec20e82099324ba2
CUmonial <indiacantwait@gmail.com>
2016-08-14 15:51:34 +0000

    join table with app modules and user category

--- demoApp/db/migrate/20160814151209_user_category_app_modules.rb
+++ demoApp/db/migrate/20160814151209_user_category_app_modules.rb
@@ -1,4 +1,16 @@
 class UserCategoryAppModules < ActiveRecord::Migration
-  def change
+  def up
+  	create_table :user_category_app_modules do |t|
+      t.integer :user_category_id
+      t.integer :app_module_id
+      
+      t.timestamps null: false
+    end
+    add_foreign_key :user_category_app_modules, :user_categories
+    add_foreign_key :user_category_app_modules, :app_modules
+  end
+
+  def down
+  	drop_table :user_category_app_modules
   end
 end
